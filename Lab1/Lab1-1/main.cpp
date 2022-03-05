4bab2a888728bb8576b38e54164ce756281dad8c
QZNF <1171100402@qq.com>
2022-03-05 16:22:19 +0800

    submit2

--- Lab1/Lab1-1/main.cpp
+++ Lab1/Lab1-1/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include<windows.h>
 #include<stdlib.h>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
 int main()
 {
-    int n=1000;
+    int n=5000;
+
+    cout<<"数组规模："<<n<<endl;
     int**A=new int*[n];
     for(int i=0;i<n;i++)
         A[i]=new int[n];
@@ -14,11 +18,13 @@ int main()
 
     for(int i=0;i<n;i++)
     {
-        a[i]=i*i;
+        srand(time(NULL));
+        a[i]=rand()%10+1;
 
         for(int j=0;j<n;j++)
             {
-                A[i][j]=i+j;
+                srand(time(NULL));
+                A[i][j]=rand()%100+1;
             }
     }
     int *sum=new int[n];
@@ -28,29 +34,43 @@ int main()
     QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
 
     QueryPerformanceCounter((LARGE_INTEGER *)&head);
-    for(int i=0;i<n;i++)
+
+
+    int c=100;//重复执行的次数，减小误差
+
+
+    for(int k=c;k>=0;k--)
     {
-        sum[i]=0;
-        for(int j=0;j<n;j++)
-            sum[i]+=A[j][i]*a[j];
+
+
+        for(int i=0;i<n;i++)
+        {
+            sum[i]=0;
+            for(int j=0;j<n;j++)
+                sum[i]+=A[j][i]*a[j];
+
+        }
 
     }
     QueryPerformanceCounter((LARGE_INTEGER *)&tail );
 
 
-    cout<<"Col:"<<(tail-head)*1000.0/freq<<"ms"<<endl;
-
-
+    cout<<"Col:"<<(tail-head)*1000.0/freq/c<<"ms"<<endl;
     QueryPerformanceFrequency((LARGE_INTEGER *)&freq );
     QueryPerformanceCounter((LARGE_INTEGER *)&head);
-    for(int i=0;i<n;i++)
-        sum[i]=0;
-    for(int j=0;j<n;j++)
-        for(int i=0;i<n;i++)
-            sum[i]=A[j][i]*a[j];
+    for(int k=c;k>=0;k--)
+    {
 
+
+
+        for(int i=0;i<n;i++)
+            sum[i]=0;
+        for(int j=0;j<n;j++)
+            for(int i=0;i<n;i++)
+                sum[i]=A[j][i]*a[j];
+    }
     QueryPerformanceCounter((LARGE_INTEGER *)&tail );
-    cout<<"Row:"<<(tail-head)*1000.0/freq<<"ms";
+    cout<<"Row:"<<(tail-head)*1000.0/freq/c<<"ms";
 
     return 0;
 }
--- Lab1/Lab1-2/main.cpp
+++ Lab1/Lab1-2/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
 #include<sys/time.h>
-
+#include<cstdlib>
+#include<stdlib.h>
+#include<ctime>
 using namespace std;
 
 int main()
 {
     int n=1000;
+
+
+    cout<<"数组规模:"<<n<<endl;
     int**A=new int*[n];
     for(int i=0;i<n;i++)
         A[i]=new int[n];
@@ -15,42 +20,59 @@ int main()
 
     for(int i=0;i<n;i++)
     {
-        a[i]=i*i;
+        srand(time(NULL));
+        a[i]=rand()%10+1;
 
         for(int j=0;j<n;j++)
             {
-                A[i][j]=i+j;
+                srand(time(NULL));
+                A[i][j]=rand()%100+1;
             }
     }
     int *sum=new int[n];
     long head,tail;
     struct timeval tv_begin,tv_end;
-    gettimeofday(&tv_begin, NULL);
-    head=tv_begin.tv_usec;
-    for(int i=0;i<n;i++)
+    //gettimeofday(&tv_begin, NULL);
+    //head=tv_begin.tv_usec;
+    clock_t start,ed;
+
+    start=clock();
+    int c=100;//循环规模
+    for(int k=c;k>=0;k--)
     {
-        sum[i]=0;
+
+
+        for(int i=0;i<n;i++)
+        {
+            sum[i]=0;
+            for(int j=0;j<n;j++)
+                sum[i]+=A[j][i]*a[j];
+
+        }
+    }
+    //gettimeofday(&tv_end,NULL);
+
+    //tail=tv_end.tv_usec;
+    ed=clock();
+    cout<<"Col:"<<(ed-start)*1000.0/c/CLOCKS_PER_SEC<<"ms"<<endl;
+
+    //gettimeofday(&tv_begin, NULL);
+    //head=tv_begin.tv_usec;
+    start=clock();
+    for(int k=c;k>=0;k--)
+    {
+
+        for(int i=0;i<n;i++)
+            sum[i]=0;
         for(int j=0;j<n;j++)
-            sum[i]+=A[j][i]*a[j];
+            for(int i=0;i<n;i++)
+                sum[i]=A[j][i]*a[j];
 
     }
-    gettimeofday(&tv_end,NULL);
-    tail=tv_end.tv_usec;
-
-    cout<<"Col:"<<(tail-head)/1000.0<<"ms"<<endl;
-
-
-    gettimeofday(&tv_begin, NULL);
-    head=tv_begin.tv_usec;
-    for(int i=0;i<n;i++)
-        sum[i]=0;
-    for(int j=0;j<n;j++)
-        for(int i=0;i<n;i++)
-            sum[i]=A[j][i]*a[j];
-    gettimeofday(&tv_end,NULL);
-    tail=tv_end.tv_usec;
-
-    cout<<"Row:"<<(tail-head)/1000.0<<"ms";
+    ed=clock();
+    //gettimeofday(&tv_end,NULL);
+    //tail=tv_end.tv_usec;
+    cout<<"Row:"<<(ed-start)*1000.0/c/CLOCKS_PER_SEC<<"ms";
 
     return 0;
 }
